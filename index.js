572e898aab1305f070ae9100b5b5d2bfc179708c
apastori <alfonso.pastori@ibm.com>
2024-09-29 23:54:32 -0300

    MovementWorking

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const board = document.querySelector(".game-board");
 const gridWidth = 20, gridHeight = 20;
 let snakePosition = [{ x: 10, y: 10}];
 let food = generateFood();
+let direction = "right";
 
 const draw = () => {
     board.innerHTML = "";
@@ -31,15 +32,57 @@ const setPosition = (snakeElement, positionSnakeElement) => {
 
 draw();
 
+//Draw Food in Board
 function drawFood() {
     const foodElement = createGameElement("div", "food");
     setPosition(foodElement, food);
     board.appendChild(foodElement);
 }
 
+// Generate random coordinates
 function generateFood() {
     const foodX = Math.floor(Math.random() * gridWidth) + 1;
     const foodY = Math.floor(Math.random() * gridHeight) + 1;
     return { x: foodX, y: foodY };
 }
+
+const moveActions = {
+    "up": (head) => {
+        return {
+            x: head.x,
+            y: head.y - 1
+        };
+    },
+    "down": (head) => {
+        return {
+            x: head.x,
+            y: head.y + 1
+        };
+    },
+    "right": (head) => {
+        return {
+            x: head.x + 1,
+            y: head.y
+        };
+    },
+    "left": (head) => {
+        return {
+            x: head.x - 1,
+            y: head.y
+        };
+    }
+}
+
+//Move Functionality
+const move = () => {
+    const snakeHead = { ...snakePosition[0] };
+    const newHead = moveActions[direction](snakeHead);
+    snakePosition.unshift(newHead);
+    snakePosition.pop();
+}
+
+setInterval(() => {
+    move();
+    draw();
+}, 200)
 
